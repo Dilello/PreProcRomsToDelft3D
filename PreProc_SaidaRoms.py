a400685ad36f5dcb86b259c31c3f4a8d40273a7d
Marcelo Di Lello Jordão <dilellocn@gmail.com>
2022-07-27 11:14:35 -0300

    Update PreProc_SaidaRoms.py
    Entendendo o interpolador

--- PreProc_SaidaRoms.py
+++ PreProc_SaidaRoms.py
@@ -14,15 +14,18 @@ df = pd.DataFrame({'mdata': mdata.flatten()}, index=index)['mdata']
 df = df.unstack(level=[0]).swaplevel().sort_index()
 df.index.names = ['CamadaSigma', 'dia']
 df1 = df.T
+df1['Long'] = posicao[:,1]
+df1['Lat'] = posicao[:,2]
 
-df1.loc[:,df1.columns.get_level_values(1).isin([0])] # df1.loc[:,df1.columns.get_level_values(Fixa a Camada = 1).isin([Valor da Camada])]
-df1.loc[:,df1.columns.get_level_values(0).isin([0])] # df1.loc[:,df1.columns.get_level_values(Fixa o Dia = 0).isin([Valor do Dia])]
+#df1.loc[:,df1.columns.get_level_values(1).isin([0])] # df1.loc[:,df1.columns.get_level_values(Fixa a Camada = 1).isin([Valor da Camada])]
+#df1.loc[:,df1.columns.get_level_values(0).isin([0])] # df1.loc[:,df1.columns.get_level_values(Fixa o Dia = 0).isin([Valor do Dia])]
 
 #arrays constituting the 3D grid
-long = np.linspace(40.1, 37.3, 100)
-lat = np.linspace(20.1, 18.2, 100)
-S = np.linspace(20.2, 35.1, 100)
+long_roms = df1.Long
+lat_roms = df1.Lat
+S = df1.loc[:,df1.columns.get_level_values(1).isin([0])]
+S_roms = S.loc[:,0]
 # evaluate the function on the points of the grid
 # points = the regular grid, #values =the data on the regular grid
 # point = the point that we want to evaluate in the 3D grid
-f = interpolate.interp2d(long, lat, S)
+f = interpolate.interp2d(long_roms, lat_roms, S_roms)
